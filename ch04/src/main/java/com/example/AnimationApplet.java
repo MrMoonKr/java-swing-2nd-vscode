f69f7d06a87d3aa1261332a392760365eeb575cd
Mr.Moon <MrMoonKr@gmail.com>
2024-12-20 19:28:10 +0900

    JApplet은 JDK 17에서는 지원하지 않음 ...

--- ch04/src/main/java/com/example/AnimationApplet.java
+++ ch04/src/main/java/com/example/AnimationApplet.java
@@ -3,11 +3,11 @@
 //
 package com.example;
 
-import javax.swing.*;
+//import javax.swing.*;
 
-public class AnimationApplet extends JApplet {
-  public void init() {
-    ImageIcon icon = new ImageIcon("images/rolling.gif");  // animated gif
-    getContentPane().add(new JLabel(icon));
-  }
-}
+// public class AnimationApplet extends JApplet {
+//   public void init() {
+//     ImageIcon icon = new ImageIcon("images/rolling.gif");  // animated gif
+//     getContentPane().add(new JLabel(icon));
+//   }
+// }
